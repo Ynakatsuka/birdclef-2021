3a1c8bd4b9b7de4633f1fef1ed374b459931f2a5
Yuki Nakatsuka <nk.tsssa@gmail.com>
2021-05-24 15:12:11 +0900

    add configs

--- src/kvt/lightning_modules/spec.py
+++ src/kvt/lightning_modules/spec.py
@@ -6,6 +6,8 @@ from .base import LightningModuleBase
 class LightningModuleSpecMixUp(LightningModuleBase):
     def training_step(self, batch, batch_nb):
         x, y = batch["x"], batch["y"]
+        aux_x = {k: v for k, v in batch.items() if (k != "x") and (k[0] == "x")}
+        aux_y = {k: v for k, v in batch.items() if (k != "y") and (k[0] == "y")}
 
         if self.transform is not None:
             x = self.transform(x)
@@ -20,12 +22,12 @@ class LightningModuleSpecMixUp(LightningModuleBase):
             and (np.random.rand() < self.storong_transform_p)
         ):
             _, y_a, y_b, lam, idx = self.strong_transform(x, y)
-            y_hat = self.forward(x, mixup_lambda=lam, mixup_index=idx)
-            loss = lam * self.hooks.loss_fn(y_hat, y_a) + (
+            y_hat = self.forward(x, mixup_lambda=lam, mixup_index=idx, **aux_x)
+            loss = lam * self.hooks.loss_fn(y_hat, y_a, **aux_y) + (
                 1 - lam
-            ) * self.hooks.loss_fn(y_hat, y_b)
+            ) * self.hooks.loss_fn(y_hat, y_b, **aux_y)
         else:
-            y_hat = self.forward(x)
-            loss = self.hooks.loss_fn(y_hat, y)
+            y_hat = self.forward(x, **aux_x)
+            loss = self.hooks.loss_fn(y_hat, y, **aux_y)
 
         return {"loss": loss}
